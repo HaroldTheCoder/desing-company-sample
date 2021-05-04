e2b9d684d16f3b344b5ddbdc5769da06efeb494c
HaroldTheCoder <80286893+HaroldTheCoder@users.noreply.github.com>
2021-05-03 23:55:08 -0400

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -2,15 +2,15 @@
 
 var modal = document.getElementById('myModal'); 
 var images = document.getElementsByClassName('img-showcase');
-var modalImg = document.getElementById("img01");
-var captionText = document.getElementById("caption");
+var modalImg = document.getElementById('img01');
+var captionText = document.getElementById('caption');
 
 
 for (var i = 0; i < images.length; i++) {
   var img = images[i];
 
   img.onclick = function(evt) {
-    modal.style.display = "block";
+    modal.style.display = 'block';
     modalImg.src = this.src;
     captionText.innerHTML = this.alt;
   }
@@ -18,29 +18,29 @@ for (var i = 0; i < images.length; i++) {
 
 //CLOSE MODAL FUNCTIONS 
 
-var span = document.getElementsByClassName("close-button")[0]; 
+var span = document.getElementsByClassName('close-button')[0]; 
 span.onclick = function() {
-	modal.style.display = "none"; 
+	modal.style.display = 'none'; 
 }
 
 
 // OPEN - CLOSE NAVBAR FUNCTION
 
  function openNav() {
-	document.querySelector(".mobile-navigation").style.display = "block";
+	document.querySelector('.mobile-navigation').style.display = 'block';
 
-	if (document.getElementById("close").src == "icon-close.svg") {
-		document.getElementById("open").src = "icon-hamburger.svg";
+	if (document.getElementById("close").src == '/images/icon-close.svg') {
+		document.getElementById("open").src = '/images/icon-hamburger.svg';
 	} else {
-		document.getElementById("close").src = "icon-close.svg";
-		document.getElementById("close").style.display = "block";
-		document.getElementById("open").style.display = "none";
+		document.getElementById("close").src = '/images/icon-close.svg';
+		document.getElementById("close").style.display = 'block';
+		document.getElementById("open").style.display = 'none';
 	}
 
  }
  function closeNav() {
-	document.querySelector(".mobile-navigation").style.display = "none";
+	document.querySelector('.mobile-navigation').style.display = 'none';
 	
-	document.getElementById("open").style.display = "block";
-	document.getElementById("close").style.display = "none";
+	document.getElementById('open').style.display = 'block';
+	document.getElementById('close').style.display = 'none';
  }
